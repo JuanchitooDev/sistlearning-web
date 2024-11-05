0c567a7e72a786004b552d458f256dfe3e3ab4ef
Juan Racchumí <juan.racchumi.dev@gmail.com>
2024-11-05 06:16:00 -0500

    Update reference to api

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3000/api'
 });
 
 export default api;
