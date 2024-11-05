d162c4b940d4b24148313bbe952778ade1558458
Juan Racchumí <juan.racchumi.dev@gmail.com>
2024-11-05 05:36:12 -0500

    Add componente Mantenimiento.vue

---
+++ src/components/Mantenimiento.vue
@@ -0,0 +1,27 @@
+<!-- src/components/Mantenimiento.vue -->
+<template>
+    <div class="flex justify-center items-center h-screen bg-gray-200">
+      <div class="text-center bg-white p-8 rounded-lg shadow-md max-w-lg mx-auto">
+        <h2 class="text-3xl font-bold text-red-600 mb-4">{{ mensaje.titulo }}</h2>
+        <p class="text-lg text-gray-700 mb-4">{{ mensaje.descripcion }}</p>
+        <p class="text-sm text-gray-500">Por favor, intente más tarde.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Mantenimiento",
+    props: {
+      mensaje: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos opcionales */
+  </style>
+  
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,38 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Mantenimiento from '../components/Mantenimiento.vue'
 import CertificadoDetail from '../components/CertificadoDetail.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
   {
     path: '/certificado/:codigo',
     name: 'certificado-detail',
     component: CertificadoDetail,
-    meta: {layout: 'no-sidebar'}
+    // meta: {layout: 'no-sidebar'}
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: Mantenimiento,
+    props: {
+      mensaje: {
+        titulo: 'Página no encontrada',
+        descripcion: 'La página que buscas no existe. Verifica la URL o vuelve al inicio'
+      }
+    }
   }
 ]
 
